1fe81540e650e789b29c24a89f295751d43bfec8
Mahesh Hiremath <mhiremat@stevens.edu>
2025-08-05 14:35:59 -0400

    added dropout and reduced no. of hidden neurons

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,14 +1,13 @@
 import torch.nn as nn
 import torch.nn.functional as F
 class MLPModel(nn.Module):
-    def __init__(self, input_dim=370*370, hidden_dim=512, num_classes=3):
+    def __init__(self, input_dim=370*370, hidden_dim=32, num_classes=3):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, num_classes)
-
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(hidden_dim, num_classes)
     def forward(self, x):
-        x = F.relu(self.fc1(x))      # First hidden layer
-        x = F.relu(self.fc2(x))      # Second hidden layer
-        x = self.fc3(x)              # Output logits for 3 classes
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)              # Output logits for 3 classes
         return x
